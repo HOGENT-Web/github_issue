3ea1074d65486dde8cbdd39a9d0fe4a2752c40a4
Pieter Van Der Helst <pieter.vanderhelst@hogent.be>
2022-02-23 17:42:47 +0100

    add sample code file to demonstrate creating an issue

---
+++ sample_code.js
@@ -0,0 +1,16 @@
+let x = '11' + 1;
+let y = '11' - 1;
+console.log(x); // 111
+console.log(y); // 10
+
+let a = { name: 'Kelly' };
+let b = a;
+b.name = 'Rudy';
+console.log(a.name); // Rudy
+
+console.log(0.2 + 0.1 === 0.3); // false
+
+let c = [1, 2, 3] + [4, 5, 6];
+console.log(c); //  "1,2,34,5,6"
+
+console.log([] === []); // false
